5c0f2908df027f1b9167818d30c94b0ece341caf
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-11-11 15:25:35 +0200

    fix filter

--- src/js/request-base.js
+++ src/js/request-base.js
@@ -1,20 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://books-backend.p.goit.global/books/";
+const BASE_URL = 'https://books-backend.p.goit.global/books/';
 
 export async function getTopBooks() {
   const url = `${BASE_URL}top-books`;
   const response = await axios.get(url);
- 
+
   return response.data;
 }
 
-const BASE_URL_CATALOG = "https://books-backend.p.goit.global/books/";
+const BASE_URL_CATALOG = 'https://books-backend.p.goit.global/books/';
 
 export async function getTopCategory(attributeValue) {
-  
   const url = `${BASE_URL_CATALOG}category?category=${attributeValue}`;
   const response = await axios.get(url);
-  
+
   return response.data;
 }
