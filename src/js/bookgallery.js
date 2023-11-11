e4b91883fd2b9d04d77efdf35f0616ead260cfc9
TarasLys <opros0609@gmail.com>
2023-11-11 17:39:37 +0200

    com

--- src/js/bookgallery.js
+++ src/js/bookgallery.js
@@ -1,11 +1,16 @@
 import Notiflix from 'notiflix';
 import { getTopBooks, getTopCategory } from './request-base';
 
+export const titleBestSellers = document.querySelector('.title_best_sellers');
 export const listGeneral = document.querySelector('.list_general');
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 // export const titleBestSellers = document.querySelector(".title_best_sellers")
 
 export const titleBestSellers = document.querySelector('.title_best_sellers');
+>>>>>>> Stashed changes
+=======
+
 >>>>>>> Stashed changes
 
 let n = 0;
