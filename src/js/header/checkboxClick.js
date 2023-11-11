b697ec7b0eb60832e6b253c0ad731f7f5bf382df
Yevhen <kaguasbest@gmail.com>
2023-11-11 09:12:38 +0200

    header start
    Create header:
    - logo
    - basket
    - checkbox
    - login-logout

---
+++ src/js/header/checkboxClick.js
@@ -0,0 +1,13 @@
+import { headeRrefs } from './headerConst';
+
+export function onCheckboxClick() {
+  // console.log(refs.checkbox.checked);
+  if (headeRrefs.checkbox.checked === true) {
+    localStorage.setItem(headeRrefs.THEME_KEY, headeRrefs.themeValueDark);
+    headeRrefs.body.classList.add('dark-theme');
+  }
+  if (headeRrefs.checkbox.checked === false) {
+    localStorage.getItem(headeRrefs.THEME_KEY, headeRrefs.themeValueLight);
+    headeRrefs.body.classList.remove('dark-theme');
+  }
+}
---
+++ src/js/header/headerConst.js
@@ -0,0 +1,7 @@
+export const headeRrefs = {
+  body: document.querySelector('body'),
+  checkbox: document.querySelector('.header-theme-checkbox'),
+  THEME_KEY: 'headerTheme',
+  themeValueLight: 'light-theme',
+  themeValueDark: 'dark-theme',
+};
