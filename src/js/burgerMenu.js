d3063199d38e3d5bd67aa71c8e3b5e9e4865e648
Oleksii-Koval <oleksii270@gmail.com>
2023-11-12 15:59:23 +0100

    15:59

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -5,6 +5,8 @@ const burgerMenuBtnEL = document.querySelector('.hamburger');
 const menuContainerEl = document.querySelector('.hamburger-overlay');
 const bodyEl = document.querySelector('body');
 const mobileMenuGalleryListEl = document.querySelector('.mob-menu-gallery-list');
+const scrollToTopBtn = document.getElementById("scrollTop");
+
 
 burgerMenuBtnEL.addEventListener('click', hamburgerToggle);
 
@@ -12,43 +14,37 @@ function hamburgerToggle() {
   burgerMenuBtnEL.classList.toggle('close');
   menuContainerEl.classList.toggle('active');
 
-    bodyEl.classList.add('menu-open');
-    if (bodyEl.classList.contains('menu-open')) {
-        getTopBooks()
-    .then(allCategories => {
-         renderGallery(allCategories)    
-    })
-    .catch(error => {
-    console.log(error);
-    });
-    } 
-    
+  bodyEl.classList.add('menu-open');  
   document.body.classList.toggle('menu-open', menuContainerEl.classList.contains('active'));
 }
 
-function renderGallery(allCategories) {
-    const allBooks = allCategories.reduce((acc, category) => {
-        return acc.concat(category.books);
-    }, []);
-    
-    const uniqueBooks = allBooks.filter((currentBook, index, array) => {
-        const filteredBooks = array.findIndex((book) => book.title === currentBook.title) === index;
-        return filteredBooks;
+getTopBooks()
+    .then(allCategories => {
+        const allBooks = allCategories.reduce((acc, category) => {
+            return acc.concat(category.books);
+        }, []);
+
+        const uniqueBooks = allBooks.filter((currentBook, index, array) => {
+            const filteredBooks = array.findIndex((book) => book.title === currentBook.title) === index;
+            return filteredBooks;
+        });
+
+        const mobMenuMarkup = uniqueBooks.map((book) => `
+<li class="mob-menu-gallery-item">
+    <img loading="lazy" class="mob-menu-image" src="${book.book_image}" alt="${book.title}" >
+</li>`).join("");
+
+        mobileMenuGalleryListEl.insertAdjacentHTML('afterbegin', mobMenuMarkup);
+        runGallery();
+    })
+    .catch(error => {
+        console.log(error);
     });
-    
-    const mobMenuMarkup = uniqueBooks.map((book) => `
-    <li class="mob-menu-gallery-item">
-        <img loading="lazy" class="mob-menu-image" src="${book.book_image}" alt="${book.title}" >
-    </li>`).join("");  
-    
-    mobileMenuGalleryListEl.insertAdjacentHTML('afterbegin', mobMenuMarkup);
-    runGallery();
-};
 
 function animateGallery(currentPosition, direction) {
     currentPosition += 0.3 * direction;
 
-    const maxPosition = -8000;
+    const maxPosition = -1500;
     const minPosition = 0;
 
   if (currentPosition <= maxPosition || currentPosition >= minPosition) {
@@ -64,22 +60,19 @@ function runGallery() {
     animateGallery(0, -1);
 };
 
+// scroll to top btn
 
+window.addEventListener("scroll", onScrollWindow);
 
-//up btn
+function onScrollWindow() {
 
-
-window.onscroll = function() {
-	const scrollToTopBtn = document.getElementById("scrollTop");
-    if (!scrollToTopBtn) {
-    	scrollToTopBtn = document.createElement("a");
-        scrollToTopBtn.id = "scrollTop";
-        scrollToTopBtn.href = "#";
-        document.body.appendChild(scrollToTopBtn);
-	}
-	scrollToTopBtn.style.display = document.documentElement.scrollTop > 300 ? "block" : "none";
-    scrollToTopBtn.onclick = (evt) => {
-    	evt.preventDefault();
-        document.documentElement.scrollTop = 0;
+  const showBtn = document.documentElement.scrollTop > 300;
+    scrollToTopBtn.classList.toggle("show", showBtn);
+   
+  scrollToTopBtn.onclick = (evt) => {
+    evt.preventDefault();
+      document.documentElement.scrollTop = 0;
+      scrollToTopBtn.classList.remove("show");
     };
-};
+    
+}
