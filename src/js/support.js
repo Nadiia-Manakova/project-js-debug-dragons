c25079ca55551910ccd7485c5edf0f3a66c3ccc9
AlexandrAAkulina <icomfy1484@gmail.com>
2023-11-12 17:51:24 +0200

    add links, support-img animation

--- src/js/support.js
+++ src/js/support.js
@@ -5,47 +5,47 @@ const supportFonds = [
         title: 'Save the Children',
         url:
             'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-        img: '../img/fund-01.png',
+        img: './img/fund-01.png',
     },
     {
         title: 'Project HOPE',
         url: 'https://www.projecthope.org/country/ukraine/',
-        img: '../img/fund-02.png',
+        img: './img/fund-02.png',
     },
     {
         title: 'UNITED24',
         url: 'https://u24.gov.ua/uk',
-        img: '../img/fund-09.png',
+        img: './img/fund-09.png',
     },
     {
         title: 'International Medical Corps',
         url: 'https://internationalmedicalcorps.org/country/ukraine/',
-        img: '../img/fund-03.png',
+        img: './img/fund-03.png',
     },
     {
         title: 'Medicins Sans Frontieres',
         url: 'https://www.msf.org/ukraine',
-        img: '../img/fund-07.png',
+        img: './img/fund-07.png',
     },
     {
         title: 'RAZOM',
         url: 'https://www.razomforukraine.org/',
-        img: '../img/fund-04.png',
+        img: './img/fund-04.png',
     },
     {
         title: 'Action against hunger',
         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-        img: '../img/fund-05.png',
+        img: './img/fund-05.png',
     },
     {
         title: 'World vision',
         url: 'https://www.wvi.org/emergencies/ukraine',
-        img: '../img/fund-08.png',
+        img: './img/fund-08.png',
     },
     {
         title: 'Serhiy Prytula Charity Foundation',
         url: 'https://prytulafoundation.org/en',
-        img: '../img/fund-06.png',
+        img: './img/fund-06.png',
     },
 ];
 
