fd0a264c8cfa96df20dbbd2557e700dc7d620fac
Yevhen <kaguasbest@gmail.com>
2023-11-13 15:14:14 +0200

    Change header V2.0
    Header:
    - adaptive
    -add style

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,20 @@
 import { onCheckboxClick } from './header/checkboxClick';
 import { headeRrefs } from './header/headerConst';
+import { setCurrentPageName } from './header/setCurrentPageName';
 
 headeRrefs.checkbox.addEventListener('click', onCheckboxClick);
+// function toggleModal() {
+//   elements.modal.classList.toggle('auth-backdrop-hidden');
+// }
+
+// headeRrefs.headerLoginBtn.addEventListener('click', toggleModal);
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const savedTheme = localStorage.getItem(headeRrefs.THEME_KEY);
+//   if (savedTheme === 'dark-theme') {
+//     document.documentElement.classList.add('dark-theme');
+//     headeRrefs.checkbox.checked = true;
+//   }
+// });
+
+setCurrentPageName();
