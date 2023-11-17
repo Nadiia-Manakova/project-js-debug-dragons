73e37caaab18d533a3e7f3bfdaa3a31a4eb417a0
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-11-17 12:46:23 +0200

    but-change-collor

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,14 @@
 import { checkLocalTheme, onCheckboxClick } from './header/checkboxClick';
 import { headeRrefs } from './header/headerConst';
-import { setCurrentPageName, setCurrentPageNameModal } from './header/setCurrentPageName';
+import {
+  setCurrentPageName,
+  setCurrentPageNameModal,
+} from './header/setCurrentPageName';
 
-setCurrentPageName();
-setCurrentPageNameModal();
+// setCurrentPageName();
+// setCurrentPageNameModal();
+document.addEventListener('DOMContentLoaded', setCurrentPageName);
+window.addEventListener('popstate', setCurrentPageName);
 
 checkLocalTheme();
 
