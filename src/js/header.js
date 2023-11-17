87c6c5d0ae25ccf08960f559d993174ddffd8e57
Yevhen <kaguasbest@gmail.com>
2023-11-17 12:39:29 +0200

    header4.0
    Make changes in style

--- src/js/header.js
+++ src/js/header.js
@@ -2,9 +2,11 @@ import { checkLocalTheme, onCheckboxClick } from './header/checkboxClick';
 import { headeRrefs } from './header/headerConst';
 import { setCurrentPageName, setCurrentPageNameModal } from './header/setCurrentPageName';
 
-setCurrentPageName();
-setCurrentPageNameModal();
+// setCurrentPageName();
+// setCurrentPageNameModal();
 
 checkLocalTheme();
+document.addEventListener('DOMContentLoaded', setCurrentPageName);
+window.addEventListener('popstate', setCurrentPageName);
 
 headeRrefs.checkbox.addEventListener('click', onCheckboxClick);
--- src/js/header/setCurrentPageName.js
+++ src/js/header/setCurrentPageName.js
@@ -6,10 +6,12 @@ export const setCurrentPageName = () => {
   if (currentPageName === '/index.html') {
     headeRrefs.headerPageHome.classList.add('header-yellow');
     headeRrefs.headerPageShopping.classList.remove('header-yellow');
+    return
   }
   if (currentPageName === '/page-2.html') {
     headeRrefs.headerPageHome.classList.remove('header-yellow');
     headeRrefs.headerPageShopping.classList.add('header-yellow');
+    return
   }
 };
 
@@ -18,9 +20,11 @@ export const setCurrentPageNameModal = () => {
   if (currentPageName === '/index.html') {
     headeRrefs.headerMobilePageHome.classList.add('header-yellow');
     headeRrefs.headerMobilePageShopping.classList.remove('header-yellow');
+    return
   }
   if (currentPageName === '/page-2.html') {
     headeRrefs.headerMobilePageHome.classList.remove('header-yellow');
     headeRrefs.headerMobilePageShopping.classList.add('header-yellow');
+    return
   }
 };
